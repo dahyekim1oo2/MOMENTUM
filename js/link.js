684cbfa9446dd9be53cf5bb060537574c03661d3
dahyekim1oo2 <97012327+dahyekim1oo2@users.noreply.github.com>
2023-01-16 11:53:39 +0900

    Update link.js

--- js/link.js
+++ js/link.js
@@ -25,9 +25,13 @@ function imgError(image){
 }
 
 function deletelink(link){
-    const div=link.target.parentElement;
-    console.log(site)
+    // 박스를 2중으로 사용했기 때문에 부모의 부모 엘리먼트를 지워야함
+    let div=link.target.parentElement.parentElement;
+    console.log(div)
     div.remove();
+    // 정보는 안에 있는 박스에 들어 있기 때문에 자식노드로 한단계 이동
+    div=div.lastElementChild ;
+    //해당 id를 갖는 배열을 삭제후 site배열을 다시 저장
     site=site.filter((sites)=>sites.id !==parseInt(div.id));
     console.log(site);
     savesite();
